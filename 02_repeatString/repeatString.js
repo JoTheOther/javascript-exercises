ceddbb35fde77157d92edbca69ef2e6b2e9645f1
JoTheOther <johannes.ley@bechtle.com>
2024-02-18 18:18:01 +0100

    Add solution for exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        let addedString = ""
+        for (let i = 0; i < num; i++) {
+            addedString += string;
+        }
+        return addedString
+    }
 };
 
 // Do not edit below this line
