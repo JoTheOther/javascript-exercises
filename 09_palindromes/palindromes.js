0059ce80948955dd9d54fc9612f653538fc32b50
JoTheOther <johannes.ley@bechtle.com>
2024-02-19 16:58:33 +0100

    Add solution for exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const cleanedString = string.replace(/[^a-z0-9]/gi, "").toLowerCase();
 
-};
+    const reversedString = cleanedString.split("").reverse().join("");
+
+    return cleanedString == reversedString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
